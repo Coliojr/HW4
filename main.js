64c62561d89027b723a8c7d4bbc55b9d5031efff
ColeRodocker <cole.rodocker@gmail.com>
2023-01-27 17:53:48 -0800

    Created the inputs, buttons, and javascript for assignemnt up to the show all button

--- main.js
+++ main.js
@@ -1,49 +1,55 @@
-document.addEventListener("DOMContentLoaded", function (event) {
+//Object Constructor
+function Workout(which, length, calories) {
+    this.which = which;
+    this.length = length;
+    this.calories = calories;
+}
+let calorieArr = [];
 
-    let origArray = [2,3,4];
+document.addEventListener("DOMContentLoaded", function (event) { 
+    //add button function
+    document.getElementById("add").addEventListener("click", function () {
+        // Groups radio items by name and then checks which one is clicked
+        let radioButtonGroup = document.getElementsByName("exercise");
+        let checkedRadio = Array.from(radioButtonGroup).find((radio) => radio.checked);
+        //Creates variables that will be entered as parameters to create a new Workout Object
+        let which = checkedRadio.value;
+        let length = document.getElementById("minutes").value;
+        let calories = 10;
 
-    document.getElementById("textbox1").value = origArray[0];
-    document.getElementById("textbox2").value = origArray[1];
-    document.getElementById("textbox3").value = origArray[2];
+        if (checkedRadio.value == "Push Ups")
+        {
+            calories = 15;
+        }
+        else if (checkedRadio.value == "Jump Rope")
+        {
+            calories = 18;
+        }
 
+        calories = calories * length;
 
-    document.getElementById("button2").addEventListener("click", function () {
-        let x = origArray.map(cube);
-        document.getElementById("textbox1").value = x[0];
-        document.getElementById("textbox2").value = x[1];
-        document.getElementById("textbox3").value = x[2];
+        calorieArr.push(new Workout(which, length, calories));
+        //Log to check work in console
+        console.log(calorieArr);
     });
+    //Most Burned button function
+    document.getElementById("mostburned").addEventListener("click", function () {
+        let burnArr = [];
+        let max = 0;
+        for (let i = 0; i < calorieArr.length; i++)
+        {
+            let current = calorieArr[i].calories;
 
-    document.getElementById("button3").addEventListener("click", button3);
+            if (current > max)
+            {
+                max = current;
+                burnArr = calorieArr[i];
+            }
+        }
+        if (burnArr.length > 0)
+        {
+        document.getElementById("burned").value = burnArr.which + ' - ' + burnArr.calories + ' calories';
+        }
+        
+    });
 });
-
-let origArray = [2,3,4];
-
-function button1Function() {
-    let x = origArray.map(square);
-    document.getElementById("textbox1").value = x[0];
-    document.getElementById("textbox2").value = x[1];
-    document.getElementById("textbox3").value = x[2]; 
-}
-
-function button3() {
-    let x = origArray.map(four);
-    document.getElementById("textbox1").value = x[0];
-    document.getElementById("textbox2").value = x[1];
-    document.getElementById("textbox3").value = x[2];
-}
-
-function square(item) {
-    let x = item ** 2;
-    return x;
-}
-
-function cube(item) {
-    let x = item ** 3;
-    return x;
-}
-
-function four(item) {
-    let x = item ** 4;
-    return x;
-}
